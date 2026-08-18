b1628a061e31af5002005a8e5c3f7374ed999b39
MonijaV <monishavel2006@gmail.com>
2026-08-18 13:49:48 +0530

    Implemented Scheduler Events,Scheduler Job

--- library_management/scheduled_tasks.py
+++ library_management/scheduled_tasks.py
@@ -6,3 +6,15 @@ def scheduler_demo():
     print("Scheduler Event is Running")
     print("Current Time:", now())
     print("==============================\n")
+
+
+from frappe.utils import now_datetime
+def test_configurable_scheduler():
+    print("====================================")
+    print("CONFIGURABLE SCHEDULER EXECUTED")
+    print(f"Time: {now_datetime()}")
+    print("====================================")
+    frappe.log_error(
+        title="Configurable Scheduler Test",
+        message=f"Scheduler executed at {now_datetime()}"
+    )
