8b6331fc8634d9fbc8abdb6845c9185c3317eff9
MonijaV <monishavel2006@gmail.com>
2026-09-07 15:49:31 +0530

    bench-cli-sitemanagement Assignment

--- library_management/commands.py
+++ library_management/commands.py
@@ -3,3 +3,12 @@ import click
 def welcome_msg():
     print("Hello from the custom Bench CLI!")
 commands=[welcome_msg]
+
+
+@click.command("hello-app")
+def hello_app():
+    print("Hello from custom command!")
+
+commands = [hello_app]
+
+    
